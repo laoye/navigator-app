5bed0df16c684ab6b1b8e7913a65d86566549413
laoye <cf850118@163.com>
2026-03-25 23:03:37 +0800

    chore: add .cxx and app/build to gitignore, remove cached build artifacts

--- android/app/.cxx/Debug/27w1o3s1/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "C:/Users/41643/.gradle/caches/8.13/transforms/06f2ed51be878d73e32c65d981a67f7d/transformed/jetified-hermes-android-250829098.0.9-debug/prefab/modules/hermesvm/libs/android.arm64-v8a/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/41643/.gradle/caches/8.13/transforms/06f2ed51be878d73e32c65d981a67f7d/transformed/jetified-hermes-android-250829098.0.9-debug/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/27w1o3s1/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-vision-camera/react-native-vision-cameraConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-vision-camera::VisionCamera)
-add_library(react-native-vision-camera::VisionCamera SHARED IMPORTED)
-set_target_properties(react-native-vision-camera::VisionCamera PROPERTIES
-    IMPORTED_LOCATION "D:/GitHub/fleetbase/navigator-app/node_modules/react-native-vision-camera/android/build/intermediates/cxx/Debug/695p82p4/obj/arm64-v8a/libVisionCamera.so"
-    INTERFACE_INCLUDE_DIRECTORIES "D:/GitHub/fleetbase/navigator-app/node_modules/react-native-vision-camera/android/build/headers/visioncamera"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
